561ff1a9132347d1f36aaff5a93e63de211e3345
picksitquick <picksitquick@gmail.com>
2023-07-30 14:55:39 +0530

    searchBox modify

--- image_fetcher/src/app/search/page.tsx
+++ image_fetcher/src/app/search/page.tsx
@@ -33,6 +33,12 @@ const Search: React.FC = () =>{
         }
     }
 
+    const [isOpen , setIsOpen] = useState(false);
+
+    const toggleDown = () => {
+        setIsOpen((prev) => !prev);
+    }
+
     return(
         <main>
             <div className="bg-purple-300 h-20 border-4 border-purple-500 rounded-lg">
@@ -55,11 +61,23 @@ const Search: React.FC = () =>{
                     <div className="mr-4 my-auto font-sans">login | SignUp</div>
                 </div>
             </div>
-            <div className=" bg-purple-400 h-16 border-2 border-purple-200 rounded-lg">
+            <div className="relative bg-purple-400 h-16 border-2 border-purple-200 rounded-lg">
                 <nav>
                     <ul className="flex justify-between mt-4 mx-4">
                         <li><Link href="/"><HomeIcon/>Home</Link></li>
-                        <li><Link href="/">Category</Link></li>
+                        <li><button onClick={toggleDown}>
+                            Categories
+                        </button>
+                            {isOpen && (
+                                <div className="absolute p-2 bg-rose-300 rounded-lg shadow-lg border-2"
+                                    style={{top:'80%', left:'27.5%',transform:'translate(-50%)'}}
+                                >
+                                    <a className="block p-2 hover:bg-rose-200 rounded-sm border-b-2" href="/">Option: 1</a>
+                                    <a className="block p-2 hover:bg-rose-200 rounded-sm border-b-2" href="/">Option: 2</a>
+                                    <a className="block p-2 hover:bg-rose-200 rounded-sm" href="/">Option: 3</a>
+                                </div>
+                            )}
+                        </li>
                         <li><Link href="/"><WhatshotIcon/>Trending</Link></li>
                         <li><Link href="/">About</Link></li>
                         <li><Link href="/">Social</Link></li>
